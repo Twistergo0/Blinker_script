64d1128d319bac5f663976aa8a9bf288430f88c4
Twistergo0 <stevo3@mein.gmx>
2024-08-09 20:24:46 +0200

    Dateien Hochgeladen

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Stevo' 
+description 'Blinkerscript'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+} 
+
+server_scripts {
+    'server.lua'
+}
+
+shared_scripts {
+    'config.lua'
+}
